3e75d4b5ec0d93eea6d176f5de3a49a6804d7cef
zhangshuo <streett@qq.com>
2019-03-28 18:17:05 +0800

    add static format

--- src/com/zhangshuo/UI/MainDialog.java
+++ src/com/zhangshuo/UI/MainDialog.java
@@ -44,7 +44,7 @@ public class MainDialog extends JDialog {
             "  %p\n" +
             "  factory %s.fromJson(Map<String,dynamic> json) => _$%sFromJson(json);\n" +
             "  Map<String, dynamic> toJson() => _$%sToJson(this);\n" +
-            "  static List<%s> fromListJson(jsonList){\n"+
+            "  static List<%s> fromListJson(List jsonList){\n"+
             "    return jsonList.map((map) {\n"+
             "      return %s.fromJson(map);\n"+
             "    }).toList();\n"+
